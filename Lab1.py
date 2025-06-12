4f3def286f88d1680064bf9df147d4a33f5e90b0
Denys-Butenko <butenko.denys228@gmail.com>
2025-06-12 06:18:47 +0300

    Add File

---
+++ Lab1.py
@@ -0,0 +1,27 @@
+class Denys:
+    def __init__(self, name=None, surname=None, birth_year=None):
+        self.name = name
+        self.surname = surname
+        self.birth_year = birth_year
+
+    def calculate_course(self, current_year=2025):
+
+# Вираховує курс студента відповідно до року народження
+
+        if self.birth_year is None:
+            return None
+        age = current_year - self.birth_year
+        course = max(1, min((age - 17), 4)) if age >= 17 else None
+        return course
+
+    def create_name_list(self):
+
+# Створює список з імені та прізвища
+
+        return [self.name, self.surname]
+
+# Приклад використання
+
+student = Denys("Денис", "Бутенко", 2008)
+print(student.calculate_course())  # Виведе курс студента
+print(student.create_name_list())  # Виведе список ["Денис", "Бутенко"]
